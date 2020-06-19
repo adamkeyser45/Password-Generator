a9a52276337fdbf018f07af87d1fec79a5feb8b4
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-06-19 15:02:03 -0500

    pass length, lower/uppercase, and numbers

--- Develop/script.js
+++ Develop/script.js
@@ -6,17 +6,56 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-
+ 
   generatePassword = function() {
-       
+     
+    // Ask for password length(complexity)
     var complexity = window.prompt("How long would you like your password to be? Type a number between 8 and 128.");
+      complexity = parseInt(complexity);
 
-    if (!complexity) {
-      window.alert("You didn't type in a correct response!");
-      return generatePassword();
+      // If complexity is null or "", ask to try again
+      if (!complexity) {
+        window.alert("You didn't type in a correct response!");
+        return generatePassword();
+      }
+      // If complexity is lower than 8 or higher than 128
+      if (complexity < 8 || complexity > 128) {
+        window.alert("Your password length must be between 8 and 128. Try again.");
+        return generatePassword();
+      }
+      // If correct length, store it to be used later
+      if (8 < complexity < 128) {
+        localStorage.setItem("PassLength", complexity);
+      }
+ 
+    // Ask for lowercase letters
+    var lowerCase = window.confirm("Would you like to use lowercase letters? OK for yes and CANCEL for no.")
+
+      if (lowerCase) {
+        localStorage.setItem("lowCase", true);
+      } else {
+        localStorage.setItem("lowCase", false);
+      }
+
+    // Ask for uppercase letters
+    var upperCase = window.confirm("Would you like to use uppercase letters? OK for yes and CANCEL for no.")
+
+      if (upperCase) {
+        localStorage.setItem("upCase", true);
+      } else {
+        localStorage.setItem("upCase", false);
+      }
+
+    // Ask for numbers
+    var numConfirm = window.confirm("Would you like to use numbers? OK for yes and CANCEL for no.")
+
+    if (numConfirm) {
+      localStorage.setItem("passNum", true);
+    } else {
+      localStorage.setItem("passNum", false);
     }
-  }
 
+  }
   var password = generatePassword();
 
 
