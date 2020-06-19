726ff8729ca73bb2f955457302f39701d172ed28
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-06-19 12:58:38 -0500

    added first password prompt

--- Develop/script.js
+++ Develop/script.js
@@ -6,8 +6,23 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+  generatePassword = function() {
+       
+    var complexity = window.prompt("How long would you like your password to be? Type a number between 8 and 128.");
+
+    if (!complexity) {
+      window.alert("You didn't type in a correct response!");
+      return generatePassword();
+    }
+  }
+
   var password = generatePassword();
+
+
   var passwordText = document.querySelector("#password");
+
+
 
   passwordText.value = password;
 
